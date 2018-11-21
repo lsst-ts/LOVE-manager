7df473854f765765e644b02534b9cbb86e662dff
José  Galaz <jdgalazm@gmail.com>
2018-11-21 19:04:44 -0300

    test subscription to one telemetry stream

--- manager/subscription/consumers.py
+++ manager/subscription/consumers.py
@@ -2,16 +2,26 @@ from channels.generic.websocket import AsyncJsonWebsocketConsumer
 import json
 
 class SubscriptionConsumer(AsyncJsonWebsocketConsumer):
-
     async def connect(self):
         self.telemetry_stream_group_names = []
 
         await self.accept()
 
-    async def disconnect(self, close_code):
-        # Leave telemetry_stream group
-        for telemetry_stream in self.telemetry_stream_group_names:
-            await self.channel_layer.group_discard(
-                telemetry_stream,
-                self.channel_name
-            )
+    async def join_telemetry_stream(self, telemetry_stream):
+        if telemetry_stream in self.telemetry_stream_group_names:
+            return
+        self.telemetry_stream_group_names.append(telemetry_stream)
+        await self.channel_layer.group_add(
+            telemetry_stream,
+            self.channel_name
+        )
+
+    async def receive_json(self, json_data):
+        data = json_data['data']
+        option = json_data['option']
+        if option and option == 'subscribe':
+            await self.join_telemetry_stream(data)
+            await self.send_json({
+                'data': 'Successfully subscribed to %s' % data
+            })
+            return
--- manager/tests/test_client_consumer.py
+++ manager/tests/test_client_consumer.py
@@ -2,19 +2,36 @@ import pytest
 from channels.testing import WebsocketCommunicator
 from manager.routing import application
 
-@pytest.mark.asyncio
-async def test_connection():
-    communicator = WebsocketCommunicator(application,  "/ws/subscription/")
-    
-    connected, subprotocol = await communicator.connect()
 
-    assert connected, 'Communicator was not connected'
+class TestClientConsumer:
+    @pytest.mark.asyncio
+    async def test_connection(self):
+        communicator = WebsocketCommunicator(application,  "/ws/subscription/")
+        
+        connected, subprotocol = await communicator.connect()
 
-    await communicator.disconnect()
+        assert connected, 'Communicator was not connected'
 
-    # import pdb; pdb.set_trace()
-    # # Test sending text
-    # await communicator.send_json(text_data='{"option": "subscribe", "data": "avoidanceRegions"}')
-    # response = await communicator.receive_from()
-    # assert response == "hello"
-    # # Close
+        await communicator.disconnect()
+        
+    @pytest.mark.asyncio
+    async def test_join_telemetry_stream(self):
+        
+        # Arrange
+        communicator = WebsocketCommunicator(application,  "/ws/subscription/")
+        
+        connected, subprotocol = await communicator.connect()
+
+        msg = {
+            "option": "subscribe", 
+            "data": "avoidanceRegions"
+            }
+
+        # Act
+        await communicator.send_json_to(msg)
+        response = await communicator.receive_json_from()
+
+        # Assert
+        assert response['data'] == 'Successfully subscribed to avoidanceRegions'
+
+        await communicator.disconnect()
