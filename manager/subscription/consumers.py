123b4f6d7b33a7e306ba2f109d9be87b94e82fd5
José  Galaz <jdgalazm@gmail.com>
2018-11-22 13:08:06 -0300

    test receive telemetry stream ok

--- manager/subscription/consumers.py
+++ manager/subscription/consumers.py
@@ -2,11 +2,6 @@ from channels.generic.websocket import AsyncJsonWebsocketConsumer
 import json
 
 class SubscriptionConsumer(AsyncJsonWebsocketConsumer):
-    async def connect(self):
-        self.telemetry_stream_group_names = []
-
-        await self.accept()
-
     async def join_telemetry_stream(self, telemetry_stream):
         if telemetry_stream in self.telemetry_stream_group_names:
             return
@@ -15,13 +10,49 @@ class SubscriptionConsumer(AsyncJsonWebsocketConsumer):
             telemetry_stream,
             self.channel_name
         )
+        
+    async def connect(self):
+        self.telemetry_stream_group_names = []
+
+        await self.accept()
+    
+    async def disconnect(self, close_code):
+        # Leave telemetry_stream group
+        for telemetry_stream in self.telemetry_stream_group_names:
+            await self.channel_layer.group_discard(
+                telemetry_stream,
+                self.channel_name
+            )        
 
     async def receive_json(self, json_data):
-        data = json_data['data']
         option = json_data['option']
+        data = json_data['data']
+
         if option and option == 'subscribe':
+            # Subscribe and send confirmation
             await self.join_telemetry_stream(data)
             await self.send_json({
                 'data': 'Successfully subscribed to %s' % data
             })
             return
+        
+        # Send data to telemetry_stream groups        
+        telemetry_in_data = data.keys()
+        for telemetry_group in telemetry_in_data:
+            await self.channel_layer.group_send(
+                telemetry_group,
+                {
+                    'type': 'subscription_data',
+                    'data': data[telemetry_group]
+                }
+            )
+
+    async def subscription_data(self, event):
+        """
+            Receive data from telemetry_stream group
+        """
+        data = event['data']
+        # Send data to WebSocket
+        await self.send(text_data=json.dumps({
+            'data': data
+        }))
--- manager/tests/test_client_consumer.py
+++ manager/tests/test_client_consumer.py
@@ -35,3 +35,46 @@ class TestClientConsumer:
         assert response['data'] == 'Successfully subscribed to avoidanceRegions'
 
         await communicator.disconnect()
+
+
+    @pytest.mark.asyncio
+    async def test_receive_telemetry_stream(self):
+        
+        # Arrange
+        communicator = WebsocketCommunicator(application,  "/ws/subscription/")
+        
+        connected, subprotocol = await communicator.connect()
+
+        subscription_msg = {
+            "option": "subscribe", 
+            "data": "avoidanceRegions"
+        }
+
+        producer_msg = {
+            "option": "somethingelse",
+            "data":{
+                "avoidanceRegions": {
+                    "avoidanceRegions": 0,
+                    "scale": 0.2633153200149536,
+                    "timestamp": 0.3877990729003341,
+                    "zero": 0.3617618978023529
+                },
+                "bulkCloud": {
+                    "bulkCloud": 0.6713680575252166,
+                    "timestamp": 0.5309269973966433
+                }
+            }
+        }
+
+      
+        # Act
+        await communicator.send_json_to(subscription_msg)
+        subscription_response = await communicator.receive_json_from()
+        
+        await communicator.send_json_to(producer_msg)
+        producer_response = await communicator.receive_json_from()
+
+        # Assert
+        assert producer_msg['data']['avoidanceRegions'] == producer_response['data']
+        
+        await communicator.disconnect()
