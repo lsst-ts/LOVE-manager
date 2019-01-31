25ca94888edd3e3fd01814b492685050079f6b7e
Sebastian <sebafehlandt@gmail.com>
2019-01-31 13:07:37 -0300

    Improve Dockerfile, use daphne instead of runserver, merge settings files

--- Dockerfile
+++ Dockerfile
@@ -9,9 +9,14 @@ WORKDIR /usr/src/love
 COPY manager/requirements.txt .
 RUN pip install -r requirements.txt
 
-# Copy source code
+# Copy source code and build project
 COPY manager .
+RUN find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
+RUN python manage.py collectstatic --noinput
 
-RUN ls -lah
+# Expose static files and port
+VOLUME /usr/src/love/static
+EXPOSE 8000
 
-CMD python manage.py runserver 0.0.0.0:8000 --settings=manager.production_settings
+# Run daphne server in runtime
+CMD daphne -b 0.0.0.0 -p 8000 manager.asgi:application
--- manager/manager/settings.py
+++ manager/manager/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import ldap
-from django_auth_ldap.config import LDAPSearch, GroupOfNamesType
+from django_auth_ldap.config import LDAPSearch
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -121,12 +121,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
 STATIC_URL = '/static/'
-REDIS_HOST = os.environ.get('REDIS_HOST', '127.0.0.1')
-REDIS_PASS = os.environ.get('REDIS_PASS', '')
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+#     os.path.join(BASE_DIR, "assets"),
+# ]
+
 # Channels
 ASGI_APPLICATION = 'manager.routing.application'
+REDIS_HOST = os.environ.get('REDIS_HOST', '127.0.0.1')
+REDIS_PASS = os.environ.get('REDIS_PASS', '')
 CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
@@ -137,10 +142,21 @@ CHANNEL_LAYERS = {
     },
 }
 
-# React
-WEBPACK_LOADER = {
-    'DEFAULT': {
-            'BUNDLE_DIR_NAME': 'bundles/',
-            'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.dev.json'),
-        }
-}
+# LDAP
+AUTHENTICATION_BACKENDS = [
+    'django_auth_ldap.backend.LDAPBackend',
+]
+
+# Baseline configuration.
+AUTH_LDAP_SERVER_URI = os.environ.get(
+    'AUTH_LDAP_SERVER_URI', 'ldap://45.79.13.50:389'
+)
+
+AUTH_LDAP_BIND_DN = ''
+AUTH_LDAP_BIND_PASSWORD = ''
+
+AUTH_LDAP_USER_SEARCH = LDAPSearch(
+    'ou=people,dc=planetexpress,dc=com',
+    ldap.SCOPE_SUBTREE,
+    '(uid=%(user)s)',
+)
