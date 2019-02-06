82a34e96bfe7f3ed5561b7ff36da0240e2ec047a
Sebastian <sebafehlandt@gmail.com>
2019-02-06 19:56:51 -0300

    Change websockets url in tests

--- manager/tests/test_client_consumer.py
+++ manager/tests/test_client_consumer.py
@@ -6,24 +6,24 @@ from manager.routing import application
 class TestClientConsumer:
     @pytest.mark.asyncio
     async def test_connection(self):
-        communicator = WebsocketCommunicator(application,  "/ws/subscription/")
-        
+        communicator = WebsocketCommunicator(application,  "/manager/ws/subscription/")
+
         connected, subprotocol = await communicator.connect()
 
         assert connected, 'Communicator was not connected'
 
         await communicator.disconnect()
-        
+
     @pytest.mark.asyncio
     async def test_join_telemetry_stream(self):
-        
+
         # Arrange
-        communicator = WebsocketCommunicator(application,  "/ws/subscription/")
-        
+        communicator = WebsocketCommunicator(application,  "/manager/ws/subscription/")
+
         connected, subprotocol = await communicator.connect()
 
         msg = {
-            "option": "subscribe", 
+            "option": "subscribe",
             "data": "avoidanceRegions"
             }
 
@@ -38,18 +38,18 @@ class TestClientConsumer:
 
     @pytest.mark.asyncio
     async def test_leave_telemetry_stream(self):
-        
+
         # Arrange
-        communicator = WebsocketCommunicator(application,  "/ws/subscription/")
-        
+        communicator = WebsocketCommunicator(application,  "/manager/ws/subscription/")
+
         connected, subprotocol = await communicator.connect()
 
         subs_msg = {
-            "option": "subscribe", 
+            "option": "subscribe",
             "data": "avoidanceRegions"
         }
         unsubs_msg = {
-            "option": "unsubscribe", 
+            "option": "unsubscribe",
             "data": "avoidanceRegions"
         }
 
@@ -67,20 +67,20 @@ class TestClientConsumer:
 
     @pytest.mark.asyncio
     async def test_receive_telemetry_stream(self):
-        
+
         # Arrange
-        communicator = WebsocketCommunicator(application,  "/ws/subscription/")
-        
+        communicator = WebsocketCommunicator(application,  "/manager/ws/subscription/")
+
         connected, subprotocol = await communicator.connect()
 
         subscription_msg = {
-            "option": "subscribe", 
+            "option": "subscribe",
             "data": "avoidanceRegions"
         }
 
         producer_msg = {
             "option": "somethingelse",
-            "data":{
+            "data": {
                 "avoidanceRegions": {
                     "avoidanceRegions": 0,
                     "scale": 0.2633153200149536,
@@ -94,35 +94,34 @@ class TestClientConsumer:
             }
         }
 
-      
         # Act
         await communicator.send_json_to(subscription_msg)
         subscription_response = await communicator.receive_json_from()
-        
+
         await communicator.send_json_to(producer_msg)
         producer_response = await communicator.receive_json_from()
 
         # Assert
         assert producer_msg['data']['avoidanceRegions'] == producer_response['data']
-        
+
         await communicator.disconnect()
 
     @pytest.mark.asyncio
     async def test_receive_all_telemetries(self):
 
         # Arrange
-        communicator = WebsocketCommunicator(application,  "/ws/subscription/")
-        
-        connected, subprotocol = await communicator.connect()        
+        communicator = WebsocketCommunicator(application,  "/manager/ws/subscription/")
+
+        connected, subprotocol = await communicator.connect()
 
         subscription_msg = {
-            "option": "subscribe", 
+            "option": "subscribe",
             "data": "all"
         }
 
         producer_msg = {
             "option": "somethingelse",
-            "data":{
+            "data": {
                 "avoidanceRegions": {
                     "avoidanceRegions": 0,
                     "scale": 0.2633153200149536,
@@ -139,13 +138,11 @@ class TestClientConsumer:
         # Act
         await communicator.send_json_to(subscription_msg)
         subscription_response = await communicator.receive_json_from()
-        
+
         await communicator.send_json_to(producer_msg)
         producer_response = await communicator.receive_json_from()
 
         # Assert
         assert producer_msg['data'] == producer_response['data']
-        
-        await communicator.disconnect()
-    
 
+        await communicator.disconnect()
