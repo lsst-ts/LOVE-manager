1e1ef586cc56a069d20624029c88ae640ec98f72
Sebastian <sebafehlandt@gmail.com>
2019-05-27 18:34:53 -0400

    Refactor to create different token for each session (login)

--- manager/api/urls.py
+++ manager/api/urls.py
@@ -1,14 +1,13 @@
 from django.conf.urls import include
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from rest_framework.authtoken import views
-from api.views import validate_token, logout
+from api.views import validate_token, logout, CustomObtainAuthToken
 
 
 router = DefaultRouter()
 
 urlpatterns = [
-    path('get-token/', views.obtain_auth_token, name='login'),
+    path('get-token/', CustomObtainAuthToken.as_view(), name='login'),
     path('validate-token/', validate_token, name='validate-token'),
     path('logout/', logout, name='logout'),
     path('auth/', include('rest_framework.urls', namespace='rest_framework')),
--- manager/api/views.py
+++ manager/api/views.py
@@ -1,8 +1,11 @@
 from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.decorators import api_view
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from api.serializers import UserSerializer
 
 
 @api_view(['GET'])
@@ -19,3 +22,20 @@ def logout(request):
     token = request._auth
     token.delete()
     return Response({'detail': 'Logout successful, Token succesfully deleted'}, status=status.HTTP_204_NO_CONTENT)
+
+
+class CustomObtainAuthToken(ObtainAuthToken):
+    """ API endpoint to obtain authorization tokens """
+
+    def post(self, request, *args, **kwargs):
+        """ Handle post requests """
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token = Token.objects.create(user=user)
+        user_data = UserSerializer(user).data
+
+        return Response({
+            'token': token.key,
+            'user_data': user_data,
+        })
