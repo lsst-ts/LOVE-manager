965bd9182841fde40f3975556b0e47414ee04630
Sebastian <sebafehlandt@gmail.com>
2019-07-12 11:02:53 -0400

    Test reception of messages from only 1 subscription

--- manager/subscription/tests/test_subscriptions.py
+++ manager/subscription/tests/test_subscriptions.py
@@ -13,6 +13,10 @@ msg_template = {
             "stream1": {
                 "stream1_param_1": {"value": 1, "dataType": "Int"},
                 "stream1_param_2": {"value": 1.02813957817852497, "dataType": "Float"}
+            },
+            "stream2": {
+                "stream1_param_1": {"value": 1, "dataType": "Int"},
+                "stream1_param_2": {"value": 1.02813957817852497, "dataType": "Float"}
             }
         })
     }
@@ -32,7 +36,8 @@ class TestSubscriptionCombinations:
 
     def setup_method(self):
         """ TestCase setup, executed before each test of the TestCase """
-        self.user = User.objects.create_user('username', password='123', email='user@user.cl')
+        self.user = User.objects.create_user(
+            'username', password='123', email='user@user.cl')
         self.token = Token.objects.create(user=self.user)
         self.url = 'manager/ws/subscription/?token={}'.format(self.token)
         if len(self.combinations) == 0:
@@ -64,7 +69,8 @@ class TestSubscriptionCombinations:
             response = await communicator.receive_json_from()
             # Assert
             assert response['data'] == \
-                'Successfully subscribed to {}-{}'.format(combination["csc"], combination["stream"])
+                'Successfully subscribed to {}-{}'.format(
+                    combination["csc"], combination["stream"])
         # Act 2 (Unsubscribe)
         for combination in self.combinations:
             msg = {
@@ -77,7 +83,8 @@ class TestSubscriptionCombinations:
             response = await communicator.receive_json_from()
             # Assert
             assert response['data'] == \
-                'Successfully unsubscribed to {}-{}'.format(combination["csc"], combination["stream"])
+                'Successfully unsubscribed to {}-{}'.format(
+                    combination["csc"], combination["stream"])
         await communicator.disconnect()
 
     @pytest.mark.asyncio
@@ -114,4 +121,47 @@ class TestSubscriptionCombinations:
             # Assert
             assert json.loads(msg['data'][combination['csc']])[combination['stream']] == \
                 response['data'][combination['csc']][combination['stream']]
+        await communicator.disconnect()
+
+    @pytest.mark.asyncio
+    @pytest.mark.django_db
+    async def test_receive_message_for_subscribed_group_only(self):
+        """ Test that clients subscribed to some groups only receive messages from those """
+        # Arrange
+        communicator = WebsocketCommunicator(application, self.url)
+        connected, subprotocol = await communicator.connect()
+        subscription_msg = {
+            'option': 'subscribe',
+            'csc': 'ScriptQueue',
+            'stream': 'stream1',
+            'category': 'telemetry'
+        }
+        await communicator.send_json_to(subscription_msg)
+        await communicator.receive_json_from()
+        # Act
+        for combination in self.combinations:
+            msg = {
+                'category': combination['category'],
+                'data': {
+                    combination['csc']: json.dumps({
+                        combination['stream']: {
+                            'stream1_param_1': {'value': 1, 'dataType': 'Int'},
+                            'stream1_param_2': {'value': 1.02813957817852497, 'dataType': 'Float'}
+                        }
+                    })
+                }
+            }
+            await communicator.send_json_to(msg)
+
+            if combination['category'] == subscription_msg['category'] and \
+                    combination['csc'] == subscription_msg['csc'] and \
+                    combination['stream'] == subscription_msg['stream']:
+                response = await communicator.receive_json_from()
+                # Assert
+                assert json.loads(msg['data'][combination['csc']])[combination['stream']] == \
+                    response['data'][combination['csc']][combination['stream']]
+            else:
+                # Assert
+                with pytest.raises(asyncio.TimeoutError):
+                    await asyncio.wait_for(communicator.receive_json_from(), timeout=0.1)
         await communicator.disconnect()
