a24590e941c8084da13383b637a5fbc5dc69111d
Sebastian <sebafehlandt@gmail.com>
2019-07-12 12:00:51 -0400

    Test that clients receive only the parts of a message corresponding to the streams they are suscribed to

--- manager/subscription/tests/test_subscriptions.py
+++ manager/subscription/tests/test_subscriptions.py
@@ -1,6 +1,7 @@
+import asyncio
 import json
 import pytest
-import asyncio
+import random
 from django.contrib.auth.models import User
 from channels.testing import WebsocketCommunicator
 from manager.routing import application
@@ -49,6 +50,41 @@ class TestSubscriptionCombinations:
                             "csc": csc,
                             "stream": stream,
                         })
+
+    def build_messages(self, category, csc, streams):
+        """ Builds and returns messages to send and expect, for testing purposes
+
+        Parameters
+        ----------
+        category: `string`
+            category of the message
+        csc: `string`
+            CSC of the message
+        streams: `[string]`
+            list of streams for which to add values in the message
+
+        Returns
+        -------
+        sent: `{}`
+            Dictionary containing the message to send in the test
+        expected: `{}`
+            Dictionary containing the expected response to be received by the client in the test
+        """
+        sent = {
+            'category': category,
+            'data': {
+                csc: json.dumps(
+                    {stream: {'value': 1.02813957817852497, 'dataType': 'Float'} for stream in streams}
+                )
+            }
+        }
+        expected = {
+            'category': category,
+            'data': {
+                csc: {stream: {'value': 1.02813957817852497, 'dataType': 'Float'} for stream in streams}
+            }
+        }
+        return sent, expected
 
     @pytest.mark.asyncio
     @pytest.mark.django_db
@@ -105,22 +141,13 @@ class TestSubscriptionCombinations:
             response = await communicator.receive_json_from()
         # Act
         for combination in self.combinations:
-            msg = {
-                'category': combination['category'],
-                'data': {
-                    combination['csc']: json.dumps({
-                        combination['stream']: {
-                            'stream1_param_1': {'value': 1, 'dataType': 'Int'},
-                            'stream1_param_2': {'value': 1.02813957817852497, 'dataType': 'Float'}
-                        }
-                    })
-                }
-            }
+            msg, expected = \
+                self.build_messages(combination['category'], combination['csc'], [combination['stream']])
+            # assert False
             await communicator.send_json_to(msg)
             response = await communicator.receive_json_from()
             # Assert
-            assert json.loads(msg['data'][combination['csc']])[combination['stream']] == \
-                response['data'][combination['csc']][combination['stream']]
+            assert response == expected
         await communicator.disconnect()
 
     @pytest.mark.asyncio
@@ -140,17 +167,8 @@ class TestSubscriptionCombinations:
         await communicator.receive_json_from()
         # Act
         for combination in self.combinations:
-            msg = {
-                'category': combination['category'],
-                'data': {
-                    combination['csc']: json.dumps({
-                        combination['stream']: {
-                            'stream1_param_1': {'value': 1, 'dataType': 'Int'},
-                            'stream1_param_2': {'value': 1.02813957817852497, 'dataType': 'Float'}
-                        }
-                    })
-                }
-            }
+            msg, expected = \
+                self.build_messages(combination['category'], combination['csc'], [combination['stream']])
             await communicator.send_json_to(msg)
 
             if combination['category'] == subscription_msg['category'] and \
@@ -158,8 +176,7 @@ class TestSubscriptionCombinations:
                     combination['stream'] == subscription_msg['stream']:
                 response = await communicator.receive_json_from()
                 # Assert
-                assert json.loads(msg['data'][combination['csc']])[combination['stream']] == \
-                    response['data'][combination['csc']][combination['stream']]
+                assert response == expected
             else:
                 # Assert
                 with pytest.raises(asyncio.TimeoutError):
@@ -172,7 +189,6 @@ class TestSubscriptionCombinations:
         """ Test that clients subscribed to some groups only receive messages from those """
         # Arrange
         communicator = WebsocketCommunicator(application, self.url)
-        print('combinations: ', len(self.combinations))
         connected, subprotocol = await communicator.connect()
         for combination in self.combinations:
             # Arrange: Subscribe to 1
@@ -182,23 +198,12 @@ class TestSubscriptionCombinations:
                 "category": combination["category"]
             }
             subscription_msg['option'] = 'subscribe'
-            print('  Subscribing to: ', subscription_msg)
             await communicator.send_json_to(subscription_msg)
             await communicator.receive_json_from()
             # Act: Send and receive all
             for combination in self.combinations:
-                msg = {
-                    'category': combination['category'],
-                    'data': {
-                        combination['csc']: json.dumps({
-                            combination['stream']: {
-                                'stream1_param_1': {'value': 1, 'dataType': 'Int'},
-                                'stream1_param_2': {'value': 1.02813957817852497, 'dataType': 'Float'}
-                            }
-                        })
-                    }
-                }
-                print('    Sending: ', msg)
+                msg, expected = \
+                    self.build_messages(combination['category'], combination['csc'], [combination['stream']])
                 await communicator.send_json_to(msg)
 
                 if combination['category'] == subscription_msg['category'] and \
@@ -206,17 +211,53 @@ class TestSubscriptionCombinations:
                         combination['stream'] == subscription_msg['stream']:
                     response = await communicator.receive_json_from()
                     # Assert: receive the one subscribed to
-                    print('      Asserting reception, combination: ', combination)
-                    assert json.loads(msg['data'][combination['csc']])[combination['stream']] == \
-                        response['data'][combination['csc']][combination['stream']]
+                    assert response == expected
                 else:
                     # Assert: not receive all the others
                     with pytest.raises(asyncio.TimeoutError):
-                        print('      Asserting not reception, combination: ', combination)
                         await asyncio.wait_for(communicator.receive_json_from(), timeout=0.1)
             # Clean: Unsubscribe from 1
             subscription_msg['option'] = 'unsubscribe'
-            print('  Unsubscribing from: ', subscription_msg)
+            await communicator.send_json_to(subscription_msg)
+            await communicator.receive_json_from()
+        await communicator.disconnect()
+
+    @pytest.mark.asyncio
+    @pytest.mark.django_db
+    async def test_receive_part_of_message_for_subscribed_groups_only(self):
+        """ Test that clients subscribed to some groups only receive the corresponding part of incoming messages """
+        # Arrange
+        communicator = WebsocketCommunicator(application, self.url)
+        connected, subprotocol = await communicator.connect()
+        for combination in self.combinations:
+            # Arrange: Subscribe to 1
+            subscription_msg = {
+                "csc": combination["csc"],
+                "stream": combination["stream"],
+                "category": combination["category"]
+            }
+            subscription_msg['option'] = 'subscribe'
+            await communicator.send_json_to(subscription_msg)
+            await communicator.receive_json_from()
+            # Act: Send the big message
+            msg, ignore = \
+                self.build_messages(combination['category'], combination['csc'], self.streams)
+            await communicator.send_json_to(msg)
+
+            if combination['category'] == subscription_msg['category'] and \
+                    combination['csc'] == subscription_msg['csc'] and \
+                    combination['stream'] == subscription_msg['stream']:
+                response = await communicator.receive_json_from()
+                # Assert: receive the one subscribed to
+                ignore, expected = \
+                    self.build_messages(combination['category'], combination['csc'], [combination['stream']])
+                assert response == expected
+            else:
+                # Assert: not receive all the others
+                with pytest.raises(asyncio.TimeoutError):
+                    await asyncio.wait_for(communicator.receive_json_from(), timeout=0.1)
+            # Clean: Unsubscribe from 1
+            subscription_msg['option'] = 'unsubscribe'
             await communicator.send_json_to(subscription_msg)
             await communicator.receive_json_from()
         await communicator.disconnect()
