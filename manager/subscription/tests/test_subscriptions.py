0cc0819f4f09b8d93d81213dcb912b61dae2e5ac
Sebastian <sebafehlandt@gmail.com>
2019-07-12 11:17:39 -0400

    Test that messages are only received for subscribed groups, for all subscribed groups

--- manager/subscription/tests/test_subscriptions.py
+++ manager/subscription/tests/test_subscriptions.py
@@ -165,3 +165,58 @@ class TestSubscriptionCombinations:
                 with pytest.raises(asyncio.TimeoutError):
                     await asyncio.wait_for(communicator.receive_json_from(), timeout=0.1)
         await communicator.disconnect()
+
+    @pytest.mark.asyncio
+    @pytest.mark.django_db
+    async def test_receive_message_for_subscribed_groups_only(self):
+        """ Test that clients subscribed to some groups only receive messages from those """
+        # Arrange
+        communicator = WebsocketCommunicator(application, self.url)
+        print('combinations: ', len(self.combinations))
+        connected, subprotocol = await communicator.connect()
+        for combination in self.combinations:
+            # Arrange: Subscribe to 1
+            subscription_msg = {
+                "csc": combination["csc"],
+                "stream": combination["stream"],
+                "category": combination["category"]
+            }
+            subscription_msg['option'] = 'subscribe'
+            print('  Subscribing to: ', subscription_msg)
+            await communicator.send_json_to(subscription_msg)
+            await communicator.receive_json_from()
+            # Act: Send and receive all
+            for combination in self.combinations:
+                msg = {
+                    'category': combination['category'],
+                    'data': {
+                        combination['csc']: json.dumps({
+                            combination['stream']: {
+                                'stream1_param_1': {'value': 1, 'dataType': 'Int'},
+                                'stream1_param_2': {'value': 1.02813957817852497, 'dataType': 'Float'}
+                            }
+                        })
+                    }
+                }
+                print('    Sending: ', msg)
+                await communicator.send_json_to(msg)
+
+                if combination['category'] == subscription_msg['category'] and \
+                        combination['csc'] == subscription_msg['csc'] and \
+                        combination['stream'] == subscription_msg['stream']:
+                    response = await communicator.receive_json_from()
+                    # Assert: receive the one subscribed to
+                    print('      Asserting reception, combination: ', combination)
+                    assert json.loads(msg['data'][combination['csc']])[combination['stream']] == \
+                        response['data'][combination['csc']][combination['stream']]
+                else:
+                    # Assert: not receive all the others
+                    with pytest.raises(asyncio.TimeoutError):
+                        print('      Asserting not reception, combination: ', combination)
+                        await asyncio.wait_for(communicator.receive_json_from(), timeout=0.1)
+            # Clean: Unsubscribe from 1
+            subscription_msg['option'] = 'unsubscribe'
+            print('  Unsubscribing from: ', subscription_msg)
+            await communicator.send_json_to(subscription_msg)
+            await communicator.receive_json_from()
+        await communicator.disconnect()
