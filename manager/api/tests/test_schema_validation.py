b1aa068b8ba2fdec039b8b2c5b55d6b900bdc546
José  Galaz <jdgalazm@gmail.com>
2020-03-02 16:18:52 -0300

    use debug=True in schemavalidationtestcase

--- manager/api/tests/test_schema_validation.py
+++ manager/api/tests/test_schema_validation.py
@@ -1,11 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
 from django.urls import reverse
 from api.models import Token
 from rest_framework.test import APIClient
 from django.contrib.auth.models import User, Permission
 import yaml
 
-
+@override_settings(DEBUG=True)
 class SchemaValidationTestCase(TestCase):
     script_schema = """
     $id: https://github.com/lsst-ts/ts_salobj/TestScript.yaml
@@ -37,29 +37,21 @@ class SchemaValidationTestCase(TestCase):
 
     def setUp(self):
         """Define the test suite setup."""
-        # Arrange:
+        # Arrange
         self.client = APIClient()
-        self.username = 'test'
-        self.password = 'password'
         self.user = User.objects.create_user(
-            username=self.username,
+            username='an user',
             password='password',
             email='test@user.cl',
             first_name='First',
             last_name='Last',
         )
-        self.user.user_permissions.add(Permission.objects.get(name='Execute Commands'))
-        self.login_url = reverse('login')
-        self.validate_token_url = reverse('validate-token')
-        self.logout_url = reverse('logout')
-        self.expected_permissions = {
-            'execute_commands': True,
-        }
-
-        data = {'username': self.username, 'password': self.password}
-        response = self.client.post(self.login_url, data, format='json')
-        token = Token.objects.filter(user__username=self.username).first()
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+        self.token = Token.objects.create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        self.user.user_permissions.add(Permission.objects.get(codename='view_view'),
+                                       Permission.objects.get(codename='add_view'),
+                                       Permission.objects.get(codename='delete_view'),
+                                       Permission.objects.get(codename='change_view'))
 
     def test_valid_config(self):
         """Test schema validation works for a valid config yaml string"""
