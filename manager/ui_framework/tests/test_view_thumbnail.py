7c5e7d7793073ad6b19039cfb82332a66720ad2c
José  Galaz <jdgalazm@gmail.com>
2020-03-02 12:13:05 -0300

    assert thumbnail url and file content from get/

--- manager/ui_framework/tests/test_view_thumbnail.py
+++ manager/ui_framework/tests/test_view_thumbnail.py
@@ -11,6 +11,7 @@ from ui_framework.models import View
 import os
 import glob
 import filecmp
+from django.test.utils import override_settings
 
 
 class ViewThumbnailTestCase(TestCase):
@@ -39,13 +40,10 @@ class ViewThumbnailTestCase(TestCase):
         for file in thumbnail_files_list:
             os.remove(file)
 
+    @override_settings(DEBUG=True)
     def test_new_view(self):
         """ Test behavior when adding a new view """
-        self.maxDiff = None
-        # data = {'username': self.username, 'password': self.password}
-        # response = self.client.post(self.login_url, data, format='json')
-        # print('hjola')
-
+        # Arrange
         # read test data (base64 string)
         old_count = View.objects.count()
         mock_location = os.path.join(os.getcwd(), 'ui_framework', 'tests', 'media', 'mock', 'test')
@@ -58,19 +56,24 @@ class ViewThumbnailTestCase(TestCase):
             "thumbnail": image_data
         }
 
+        # Act
         # send POST request with data
         request_url = reverse('view-list')
         response = self.client.post(request_url, request_data, format='json')
 
-        # assert response status code 201
+        # Assert
+        # - response status code 201
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-        # assert new object was created
+        # - new object was created
         new_count = View.objects.count()
         self.assertEqual(old_count + 1, new_count)
 
-        # assert expected response data
+        # - thumbnail url
         view = View.objects.get(name="view name")
+        self.assertEqual(view.thumbnail.url, '/media/thumbnails/view_1.png')
+
+        # - expected response data
         expected_response = {
             'id': view.id,
             'name': 'view name',
@@ -79,8 +82,17 @@ class ViewThumbnailTestCase(TestCase):
         }
         self.assertEqual(response.data, expected_response)
 
-        # assert stored image file content
+        # - stored file content
         file_url = settings.MEDIA_BASE + view.thumbnail.url
         expected_url = mock_location + '.png'
-        self.assertEqual(filecmp.cmp(file_url, expected_url),
+        self.assertTrue(filecmp.cmp(file_url, expected_url),
          f'\nThe image was not saved as expected\nsaved at {file_url}\nexpected at {expected_url}')
+
+        # - retrieved file content from get/ endpoint
+        with open(file_url, 'rb') as f:
+            file_content = f.read()
+            png_response = self.client.get('/manager' + view.thumbnail.url)
+            stream = b''.join(png_response.streaming_content)
+            print(stream, flush=True)
+            print(png_response, flush=True)
+            self.assertEqual(stream, file_content)
