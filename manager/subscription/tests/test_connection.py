57541bffd52c388253b359c6f29c4ce4552798db
Sebastián Fehlandt <sfehlandt@Inria-MacBook-Pro-SF.local>
2020-03-12 17:11:45 -0300

    Fix auth and some tests

--- manager/subscription/tests/test_connection.py
+++ manager/subscription/tests/test_connection.py
@@ -2,6 +2,7 @@
 import pytest
 import asyncio
 from django.contrib.auth.models import User
+from channels.db import database_sync_to_async
 from channels.testing import WebsocketCommunicator
 from channels.layers import get_channel_layer
 from manager.routing import application
@@ -12,20 +13,35 @@ from api.models import Token
 class TestClientConnection:
     """Test that clients can or cannot connect depending on different conditions."""
 
+    @pytest.mark.django_db
+    @database_sync_to_async
+    def setup(self):
+        self.user = User.objects.create_user('username', password='123', email='user@user.cl')
+        self.token = Token.objects.create(user=self.user)
+        self.user2 = User.objects.create_user('username2', password='123', email='user@user.cl')
+        self.token2 = Token.objects.create(user=self.user2)
+
+
+    @pytest.mark.django_db
+    @database_sync_to_async
+    def destroy(self):
+        User.objects.all().delete()
+        Token.objects.all().delete()
+
     @pytest.mark.asyncio
     @pytest.mark.django_db
     async def test_connection_with_token(self):
         """Test that clients can connect with a valid token."""
         # Arrange
-        user = User.objects.create_user('username', password='123', email='user@user.cl')
-        token = Token.objects.create(user=user)
-        url = 'manager/ws/subscription/?token={}'.format(token)
+        await self.setup()
+        url = 'manager/ws/subscription/?token={}'.format(self.token)
         communicator = WebsocketCommunicator(application, url)
         # Act
         connected, subprotocol = await communicator.connect()
         # Assert
         assert connected, 'Communicator was not connected'
         await communicator.disconnect()
+        await self.destroy()
 
     @pytest.mark.asyncio
     @pytest.mark.django_db
@@ -46,15 +62,15 @@ class TestClientConnection:
     async def test_connection_failed_for_invalid_token(self):
         """Test that clients cannot connect with an invalid token."""
         # Arrange
-        user = User.objects.create_user('username', password='123', email='user@user.cl')
-        token = Token.objects.create(user=user)
-        url = 'manager/ws/subscription/?token={}'.format(str(token) + 'fake')
+        await self.setup()
+        url = 'manager/ws/subscription/?token={}'.format(str(self.token) + 'fake')
         communicator = WebsocketCommunicator(application, url)
         # Act
         connected, subprotocol = await communicator.connect()
         # Assert
         assert not connected, 'Communicator should not have connected'
         await communicator.disconnect()
+        await self.destroy()
 
     @pytest.mark.asyncio
     @pytest.mark.django_db
@@ -75,6 +91,7 @@ class TestClientConnection:
     async def test_connection_interrupted_when_logout_message_is_sent(self):
         """Test that a client gets disconnected when a message is sent for it to logout, for only that client"""
         # ARRANGE
+        await self.setup()
         password = PROCESS_CONNECTION_PASS
         subscription_msg = {
             "option": "subscribe",
@@ -87,12 +104,8 @@ class TestClientConnection:
         channel_layer = get_channel_layer()
 
         # Connect 3 clients (2 users and 1 with password)
-        user = User.objects.create_user('username', password='123', email='user@user.cl')
-        user2 = User.objects.create_user('username2', password='123', email='user@user.cl')
-        token = Token.objects.create(user=user)
-        token2 = Token.objects.create(user=user2)
-        client1 = WebsocketCommunicator(application, 'manager/ws/subscription/?token={}'.format(token))
-        client2 = WebsocketCommunicator(application, 'manager/ws/subscription/?token={}'.format(token2))
+        client1 = WebsocketCommunicator(application, 'manager/ws/subscription/?token={}'.format(self.token))
+        client2 = WebsocketCommunicator(application, 'manager/ws/subscription/?token={}'.format(self.token2))
         client3 = WebsocketCommunicator(application, 'manager/ws/subscription/?password={}'.format(password))
         for client in [client1, client2, client3]:
             connected, subprotocol = await client.connect()
@@ -100,7 +113,7 @@ class TestClientConnection:
 
         # ACT
         await channel_layer.group_send(
-            'token-{}'.format(str(token)),
+            'token-{}'.format(str(self.token)),
             {'type': 'logout', 'message': ''}
         )
         await asyncio.sleep(1)  # Wait 1 second, to ensure the connection is closed before we continue
@@ -125,12 +138,18 @@ class TestClientConnection:
         await client1.disconnect()
         await client2.disconnect()
         await client3.disconnect()
+        await self.destroy()
+
+    @database_sync_to_async
+    def delete_token(self):
+        self.token.delete()
 
     @pytest.mark.asyncio
     @pytest.mark.django_db(transaction=True)
     async def test_connection_interrupted_when_token_is_deleted(self):
         """Test that a client gets disconnected when the token is deleted from the database"""
         # ARRANGE
+        await self.setup()
         password = PROCESS_CONNECTION_PASS
         subscription_msg = {
             "option": "subscribe",
@@ -142,19 +161,15 @@ class TestClientConnection:
         expected_response = 'Successfully subscribed to event-ScriptQueue-0-stream1'
 
         # Connect 3 clients (2 users and 1 with password)
-        user = User.objects.create_user('username', password='123', email='user@user.cl')
-        user2 = User.objects.create_user('username2', password='123', email='user@user.cl')
-        token = Token.objects.create(user=user)
-        token2 = Token.objects.create(user=user2)
-        client1 = WebsocketCommunicator(application, 'manager/ws/subscription/?token={}'.format(token))
-        client2 = WebsocketCommunicator(application, 'manager/ws/subscription/?token={}'.format(token2))
+        client1 = WebsocketCommunicator(application, 'manager/ws/subscription/?token={}'.format(self.token))
+        client2 = WebsocketCommunicator(application, 'manager/ws/subscription/?token={}'.format(self.token2))
         client3 = WebsocketCommunicator(application, 'manager/ws/subscription/?password={}'.format(password))
         for client in [client1, client2, client3]:
             connected, subprotocol = await client.connect()
             assert connected, 'Error, client was not connected, test could not be completed'
 
         # ACT: delete de token
-        token.delete()
+        await self.delete_token()
         await asyncio.sleep(1)  # Wait 1 second, to ensure the connection is closed before we continue
 
         # ASSERT
@@ -177,3 +192,4 @@ class TestClientConnection:
         await client1.disconnect()
         await client2.disconnect()
         await client3.disconnect()
+        await self.destroy()
