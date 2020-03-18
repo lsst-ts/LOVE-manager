b4a3771b7e9be77b02cd33f15da7f9c93d334a2b
Sebastian Fehlandt <sfehlandt@Sebastians-MacBook-Pro.local>
2020-03-18 17:12:49 -0300

    Fix error when migrations fail because the database is not ready

--- manager/runserver.sh
+++ manager/runserver.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+echo "Making migrations"
+while ! python manage.py makemigrations
+do
+  echo Sleeping 1 second waiting for database ${DB_HOST} ${DB_PORT}
+  sleep 1
+done
+echo "Conected to ${DB_HOST} ${DB_PORT}"
 
-python manage.py makemigrations
+echo "Applying migrations"
 python manage.py migrate
 
+echo "Creating default users"
 python manage.py createusers --adminpass ${ADMIN_USER_PASS} --userpass ${USER_USER_PASS} --cmduserpass ${CMD_USER_PASS}
+echo "Applying fixtures"
 python manage.py loaddata ui_framework/fixtures/initial_data.json
+
+echo "Starting server"
 daphne -b 0.0.0.0 -p 8000 manager.asgi:application
