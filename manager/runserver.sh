cf43f1948b76006b1bc38e600f51c076eb348fc7
Sebastian Fehlandt <sfehlandt@Sebastians-MacBook-Pro.local>
2020-03-18 18:17:28 -0300

    Improve logs in runserver.sh

--- manager/runserver.sh
+++ manager/runserver.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-echo "Making migrations"
+echo "\nMaking migrations"
 while ! python manage.py makemigrations
 do
-  echo Sleeping 1 second waiting for database ${DB_HOST} ${DB_PORT}
+  echo "Sleeping 1 second waiting for database ${DB_HOST} ${DB_PORT}"
   sleep 1
 done
-echo "Conected to ${DB_HOST} ${DB_PORT}"
+echo "\nConected to ${DB_HOST} ${DB_PORT}"
 
-echo "Applying migrations"
+echo "\nApplying migrations"
 python manage.py migrate
 
-echo "Creating default users"
+echo "\nCreating default users"
 python manage.py createusers --adminpass ${ADMIN_USER_PASS} --userpass ${USER_USER_PASS} --cmduserpass ${CMD_USER_PASS}
-echo "Applying fixtures"
+echo "\nApplying fixtures"
 mkdir -p media/thumbnails
 cp -u ui_framework/fixtures/thumbnails/* media/thumbnails
 python manage.py loaddata ui_framework/fixtures/initial_data.json
 
-echo "Starting server"
+echo "\nStarting server"
 daphne -b 0.0.0.0 -p 8000 manager.asgi:application
