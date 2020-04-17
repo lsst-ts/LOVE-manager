26d8b544f7d28acc2bfd3cb2a33b78c1d79b24a4
Sebastián Fehlandt <sebafehlandt@gmail.com>
2020-04-17 13:13:23 -0400

    Add get_times function to utils

--- manager/manager/utils.py
+++ manager/manager/utils.py
@@ -12,3 +12,19 @@ def tai_utc_offset() -> float:
     t = Time.now()
     dt = t.datetime.timestamp() - t.tai.datetime.timestamp()
     return dt
+
+
+def get_times():
+    t = Time.now()
+    t_utc = t.datetime.timestamp()
+    t_tai = t.tai.datetime.timestamp()
+    sidereal_summit = t.sidereal_time("apparent", longitude=-70.749417, model=None)
+    sidereal_greenwich = t.sidereal_time("apparent", longitude="greenwich", model=None)
+    return {
+        "utc": t_utc,
+        "tai": t_tai,
+        "mjd": t.mjd,
+        "sidereal_summit": sidereal_summit.value,
+        "sidereal_greenwich": sidereal_greenwich.value,
+        "tai_to_utc": t_utc - t_tai,
+    }
