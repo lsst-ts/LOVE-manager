840232766c5738f1ddd835103d93b4ef5ec2f763
sebastian-aranda <aranda.sebastian@gmail.com>
2021-10-21 12:58:40 -0300

    cast redis config variables to int

--- manager/manager/settings.py
+++ manager/manager/settings.py
@@ -206,8 +206,8 @@ ASGI_APPLICATION = "manager.routing.application"
 REDIS_HOST = os.environ.get("REDIS_HOST", False)
 REDIS_PORT = os.environ.get("REDIS_PORT", "6379")
 REDIS_PASS = os.environ.get("REDIS_PASS", False)
-REDIS_CONFIG_EXPIRY = os.environ.get("REDIS_CONFIG_EXPIRY", 60)
-REDIS_CONFIG_CAPACITY = os.environ.get("REDIS_CONFIG_CAPACITY", 100)
+REDIS_CONFIG_EXPIRY = int(os.environ.get("REDIS_CONFIG_EXPIRY", 60))
+REDIS_CONFIG_CAPACITY = int(os.environ.get("REDIS_CONFIG_CAPACITY", 100))
 if REDIS_HOST and not TESTING:
     CHANNEL_LAYERS = {
         "default": {
