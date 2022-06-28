3c4bd8b079d6deaf32c717d7ed8a746e0dd9a057
Sebastian Aranda <aranda.sebastian@gmail.com>
2022-06-28 15:56:31 -0400

    Refactor JIRA ticket creation condition

--- manager/api/views.py
+++ manager/api/views.py
@@ -1330,7 +1330,7 @@ class ExposurelogViewSet(viewsets.ViewSet):
             lfa_urls = lfa_response.data.get("urls")
 
         jira_url = None
-        if "jira" in request.data:
+        if request.data.get("jira", False):
             request.data._mutable = True
             request.data["lfa_files_urls"] = lfa_urls
             request.data._mutable = False
@@ -1403,7 +1403,7 @@ class NarrativelogViewSet(viewsets.ViewSet):
             lfa_urls = lfa_response.data.get("urls")
 
         jira_url = None
-        if "jira" in request.data:
+        if request.data.get("jira", False):
             request.data._mutable = True
             request.data["lfa_files_urls"] = lfa_urls
             request.data._mutable = False
