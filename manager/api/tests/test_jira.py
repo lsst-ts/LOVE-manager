cf7d7d761f13c007f4b084482992c018796300b6
Astrid Reyes De La Rosa <astrid.reyes@inria.cl>
2022-07-04 18:07:13 -0400

    Jira test more complete but with some errors

--- manager/api/tests/test_jira.py
+++ manager/api/tests/test_jira.py
@@ -1,4 +1,5 @@
 import requests
+import random
 from unittest.mock import patch
 from django.test import TestCase, override_settings
 from api.views import jira
@@ -9,10 +10,126 @@ class JiraTestCase(TestCase):
     def setUp(self):
         """Define the test suite setup."""
         # Arrange
-        self.jira_request = requests.Request(data={"test": "test"})
+        shared_params = [
+            "level",
+            "lfa_files_urls",
+            "message_text",
+            "user_id",
+            "user_agent",
+        ]
+        exposure_params = ["obs_id", "instrument", "exposure_flag"]
+        narrative_params = [
+            "subsystem",
+            "csc",
+            "salindex",
+            "topic",
+            "parameter",
+            "begin_date",
+            "end_date",
+            "time_lost",
+        ]
+
+        request_shared = {
+            "level": 10,
+            "lfa_files_urls": ["test", "test"],
+            "message_text": "Lorem ipsum",
+            "user_id": "love@localhost",
+            "user_agent": "LOVE",
+        }
+
+        request_exposure = {
+            "obs_id": 1000,
+            "instrument": "LATISS",
+            "exposure_flag": "none",
+        }
+
+        request_narrative = {
+            "subsystem": "MainTel",
+            "csc": "M1M3",
+            "salindex": "1",
+            "topic": "accelerometerData",
+            "parameter": "accelerometer",
+            "begin_date": "202200703-19:58:13",
+            "end_date": "20220704-19:25:13",
+            "time_lost": 10,
+        }
+
+        request_full_exposure = {
+            **request_shared,
+            **request_exposure,
+            "request_type": "exposure",
+        }
+        request_full_narrative = {
+            **request_shared,
+            **request_narrative,
+            "request_type": "narrative",
+        }
+
+        # exposure
+        data_exposure_without_request_type = {**request_full_exposure}
+        del data_exposure_without_request_type["request_type"]
+        self.jira_request_exposure_without_request_type = requests.Request(
+            data=data_exposure_without_request_type
+        )
+
+        data_exposure_without_shared_param = {**request_full_exposure}
+        del data_exposure_without_shared_param[random.choice(shared_params)]
+        self.jira_request_exposure_without_shared_param = requests.Request(
+            data=data_exposure_without_shared_param
+        )
+
+        data_exposure_without_param = {**request_full_exposure}
+        del data_exposure_without_param[random.choice(exposure_params)]
+        self.jira_request_exposure_without_param = requests.Request(
+            data=data_exposure_without_param
+        )
+
+        # narrative
+        data_narrative_without_request_type = {**request_full_narrative}
+        del data_narrative_without_request_type["request_type"]
+        self.jira_request_narrative_without_request_type = requests.Request(
+            data=data_narrative_without_request_type
+        )
+
+        data_narrative_without_shared_param = {**request_full_narrative}
+        del data_narrative_without_shared_param[random.choice(shared_params)]
+        self.jira_request_narrative_without_shared_param = requests.Request(
+            data=data_narrative_without_shared_param
+        )
+
+        data_narrative_without_param = {**request_full_narrative}
+        del data_narrative_without_param[random.choice(narrative_params)]
+        self.jira_request_narrative_without_param = requests.Request(
+            data=data_narrative_without_param
+        )
+
+        self.jira_request_exposure_full = requests.Request(data=request_full_exposure)
+        self.jira_request_narrative_full = requests.Request(data=request_full_narrative)
 
     def test_missing_parameters(self):
         """Test call to function with missing parameters"""
+
+        jira_response = jira(
+            self.jira_request_exposure_without_request_type
+        )  # retornar error -> assert
+        assert jira_response.data["ack"] == "Error"
+
+        jira_response = jira(
+            self.jira_request_exposure_without_shared_param
+        )  # retornar error -> assert
+        assert jira_response.data["ack"] == "Error"
+
+        jira_response = jira(
+            self.jira_request_exposure_without_param
+        )  # retornar error -> assert
+        assert jira_response.data["ack"] == "Error B"
+
+        jira_response = jira(
+            self.jira_request_narrative_without_param
+        )  # retornar error -> assert
+        assert jira_response.data["ack"] == "Error C"
+
+        jira(self.jira_request)
         assert "true" == "true"
 
     def test_needed_parameters(self):
@@ -23,8 +140,15 @@ class JiraTestCase(TestCase):
 
         print(self.jira_request.data)
 
-        jira_response = jira(self.jira_request)
+        jira_response = jira(self.jira_request_exposure_full)
         print(jira_response)
+        assert jira_response.status_code == 200
+        assert jira_response.data["ack"] == "Jira ticket created"
+        assert jira_response.data["url"] == "https://jira.lsstcorp.org/browse/LOVE-XX"
 
-        assert "true" == "true"
+        jira_response = jira(self.jira_request_narrative_full)
+        assert jira_response.status_code == 200
+        assert jira_response.data["ack"] == "Jira ticket created"
+        assert jira_response.data["url"] == "https://jira.lsstcorp.org/browse/LOVE-XX"
+
         mock_jira_patcher.stop()
