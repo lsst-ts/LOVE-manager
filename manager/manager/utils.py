50d2082cebbb3aa90b8bfba485d3a0bba94c8f75
Sebastian Aranda Sanchez <aranda.sebastian@gmail.com>
2024-03-26 18:25:28 -0300

    Replace `\n` by `<br>` on night report email arrangement

--- manager/manager/utils.py
+++ manager/manager/utils.py
@@ -829,6 +829,7 @@ def arrange_nightreport_email(report, plain=False):
         """
         return plain_content
 
+    new_line_character = "\n"
     html_content = f"""
     <!DOCTYPE html>
     <html lang="en">
@@ -844,12 +845,12 @@ def arrange_nightreport_email(report, plain=False):
         <p>
             Summary:
             <br>
-            {report["summary"]}
+            {report["summary"].replace(new_line_character, '<br>')}
         </p>
         <p>
             Final telescope status:
             <br>
-            {report["telescope_status"]}
+            {report["telescope_status"].replace(new_line_character, '<br>')}
         </p>
         <p>
             Additional resources:
