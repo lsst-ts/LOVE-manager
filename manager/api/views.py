c4e18ff2157b47082a810cdc990a584d74081c04
Sebastian Aranda <aranda.sebastian@gmail.com>
2024-12-17 09:18:12 -0300

    temporal commit.

--- manager/api/views.py
+++ manager/api/views.py
@@ -1317,6 +1317,10 @@ class NarrativelogViewSet(viewsets.ViewSet):
             if key in json_data:
                 json_data[key] = json_data[key].split(",")
 
+        # Transform components_json to a dict
+        if "components_json" in json_data:
+            json_data["components_json"] = json.loads(json_data["components_json"])
+
         # Add LFA and JIRA urls to the payload
         json_data["urls"] = [jira_url, *lfa_urls]
         json_data["urls"] = list(filter(None, json_data["urls"]))
@@ -1374,6 +1378,10 @@ class NarrativelogViewSet(viewsets.ViewSet):
         for key in array_keys:
             if key in json_data:
                 json_data[key] = json_data[key].split(",")
+
+        # Transform components_json to a dict
+        if "components_json" in json_data:
+            json_data["components_json"] = json.loads(json_data["components_json"])
 
         # Add LFA and JIRA urls urls to the payload
         json_data["urls"] = [
